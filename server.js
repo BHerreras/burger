48dec5823b7de93c7f713a34bc7caa3a7f61333f
Bri <bherreras23@gmail.com>
2018-05-26 14:00:08 -0700

    New Stylin & Btns

--- server.js
+++ server.js
@@ -2,29 +2,25 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 3000;
-
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// parse application
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var router = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burgers_Controller.js");
 
-app.use(router);
+app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
-	console.log("Server listening on: http://localhost:" + PORT);
+	console.log("Listening on port:%s", PORT);
 });
